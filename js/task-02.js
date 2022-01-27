fecb7b2affa9373985cb0875d0e3f8dc4f029990
Viktoriia Melnyk <mviktoria1005@gmail.com>
2022-01-27 09:25:02 +0200

    task-02 done

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,19 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsList = document.querySelector('#ingredients');
+
+const ingredientItemsEl = ingredients.map(ingredient => {
+  const itemEl = document.createElement('li');
+  itemEl.classList.add('item');
+  itemEl.textContent = ingredient;
+  
+  return itemEl;
+})
+
+ingredientsList.append(...ingredientItemsEl);
+
+const result = document.querySelector('#ingredients');
+console.log(result);
+
