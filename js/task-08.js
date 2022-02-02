adfd654000ed66812c105cf1de57fd0a4d4a50ed
Viktoriia Melnyk <mviktoria1005@gmail.com>
2022-02-02 16:08:29 +0200

    changes task-08

--- js/task-08.js
+++ js/task-08.js
@@ -4,6 +4,30 @@ const form = document.querySelector('.login-form');
 form.addEventListener('submit', onButtonSubmit);
 
 function onButtonSubmit(event) {
+    // отключить перезагрузку страницы
+
 event.preventDefault();
-console.dir(event.currentTarget.elements)
+
+    const formElements = event.currentTarget.elements;
+    const mail = formElements.email.value;
+    const password = formElements.password.value;
+
+// проверка на заполнение полей
+
+if(mail === '' || password === '') {
+    alert ('Убедитесь, что все поля формы заполнены');
+    form.reset();
+    return;
+}
+
+// собрать данные с формы 
+
+const formData = new FormData(event.currentTarget);
+
+formData.forEach((value, name) => {
+    
+});
+console.log(formData);
+
+form.reset();
 }
