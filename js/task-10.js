d490c5bab2d94b8b9ed2559e02b86590b37b26af
Viktoriia Melnyk <mviktoria1005@gmail.com>
2022-02-04 01:05:56 +0200

    test task-10

--- js/task-10.js
+++ js/task-10.js
@@ -1,10 +1,72 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+
+
+// const input = document.querySelector("#controls input");
+// const createBoxBtn = document.querySelector('[data-create]');
+// const destroyBoxBtn = document.querySelector('[data-destroy]');
+// const boxes = document.querySelector("#boxes");
+
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215)
+//     .toString(16)
+//     .padStart(6, 0)}`;
+// }
+
+// input.addEventListener("input", getAmount);
+
+// function getAmount() {
+//   var amount = +document.querySelector("#controls input").value;
+//   createBox(amount);
+// }
+
+// createBoxBtn.addEventListener('click', createBox);
+
+// function createBox(amount) {
+//   let defaultBoxSize = 30;
+//   let fragment = document.createDocumentFragment();
+//   for (let i = 0; i < amount; i+=1) {
+//     let size = defaultBoxSize + i * 10;
+//     let div = document.createElement("div");
+//     div.style.cssText = `width: ${size}px; height: ${size}px; margin: 5px; background-color: ${getRandomHexColor()}`;
+//     fragment.appendChild(div);
+//   }
+//   boxes.appendChild(fragment);
+// }
+
+// destroyBoxBtn.addEventListener('click', destroyBox);
+
+// function destroyBox() {
+//   boxes.innerHTML = "";
+// }
+
+
+
+var render = document.querySelector('[data-create]');
+var destroy = document.querySelector('[data-destroy]');
+var boxes = document.getElementById("boxes");
+render.addEventListener("click", getAmount);
+destroy.addEventListener("click", destroyBoxes);
+
+function getAmount() {
+  var amount = +document.querySelector("#controls input").value;
+  createBoxes(amount);
 }
 
-const input = document.querySelector("#controls input");
-const createBoxBtn = document.querySelector('[data-create]');
-const destroyBoxBtn = document.querySelector('[data-destroy]');
-const boxes = document.querySelector("#boxes");
+function createBoxes(amount) {
+  var basicSize = 30;
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < amount; i++) {
+    var size = basicSize + i * 10;
+    var div = document.createElement("div");
+    div.style.cssText = `width: ${size}px; height: ${size}px; background-color: rgba( ${random()} , ${random()} , ${random()} )`;
+    fragment.appendChild(div);
+  }
+  boxes.appendChild(fragment);
+}
+
+function destroyBoxes() {
+  boxes.innerHTML = "";
+}
+
+function random() {
+  return Math.floor(Math.random() * 256);
+}
