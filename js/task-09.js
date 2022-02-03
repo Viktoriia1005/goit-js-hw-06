a8972d79d3bd44c265162e58bfa51a0072a1b6e5
Viktoriia Melnyk <mviktoria1005@gmail.com>
2022-02-03 18:45:38 +0200

    task-10 started

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,20 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+
+
+const changeColorBtn = document.querySelector('.change-color');
+
+changeColorBtn.addEventListener('click', onchangeColorBtn);
+
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215)
+//     .toString(16)
+//     .padStart(6, 0)}`;
+// }
+const getRandomHexColor = `#${Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, 0)}`
+
+function onchangeColorBtn () {
+  
+  document.body.style.backgroundColor = getRandomHexColor;
+  document.querySelector('.color').innerHTML = getRandomHexColor;
 }
--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,10 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
+
+const input = document.querySelector("#controls input");
+const createBoxBtn = document.querySelector('[data-create]');
+const destroyBoxBtn = document.querySelector('[data-destroy]');
+const boxes = document.querySelector("#boxes");
