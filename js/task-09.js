cbbec10b2ed484cf11457e1f6554d5ba1913c79d
Viktoriia Melnyk <mviktoria1005@gmail.com>
2022-02-03 23:20:40 +0200

    task-09 done

--- js/task-09.js
+++ js/task-09.js
@@ -4,17 +4,14 @@ const changeColorBtn = document.querySelector('.change-color');
 
 changeColorBtn.addEventListener('click', onchangeColorBtn);
 
-// function getRandomHexColor() {
-//   return `#${Math.floor(Math.random() * 16777215)
-//     .toString(16)
-//     .padStart(6, 0)}`;
-// }
-const getRandomHexColor = `#${Math.floor(Math.random() * 16777215)
-      .toString(16)
-      .padStart(6, 0)}`
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 
 function onchangeColorBtn () {
   
-  document.body.style.backgroundColor = getRandomHexColor;
-  document.querySelector('.color').innerHTML = getRandomHexColor;
+  document.body.style.backgroundColor = getRandomHexColor();
+  document.querySelector('.color').innerText = getRandomHexColor();
 }
