bfe573d6d5f4f458c59c966fc04188b2fe665511
Viktoriia Melnyk <mviktoria1005@gmail.com>
2022-02-08 08:04:09 +0200

    Done

--- js/task-05.js
+++ js/task-05.js
@@ -2,11 +2,11 @@ const input = document.querySelector('input#name-input');
 const nameLabel = document.querySelector('span#name-output');
 
 function onInputChange (event) {
+
+    event.currentTarget.value.trim() === '' ?
+    nameLabel.textContent = 'Anonymous' :
     nameLabel.textContent = event.currentTarget.value;
 
-    if (event.currentTarget.value.trim() === '') {
-        nameLabel.textContent = 'Anonymous';
-    }
 } 
 
 input.addEventListener('input', onInputChange);
