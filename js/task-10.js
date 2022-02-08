2147d5e6d1109ec2cd4b94a41fa333d12fc00fef
Viktoriia Melnyk <mviktoria1005@gmail.com>
2022-02-08 15:26:37 +0200

    done task-10

--- js/task-10.js
+++ js/task-10.js
@@ -11,20 +11,21 @@ function getAmount() {
 let basicSize = 30;
 
 function createBoxes(amount) {
-  
+  let boxSize = 0;
   const fragment = document.createDocumentFragment();
   for (let i = 0; i < amount; i+=1) {
-    let size = basicSize + i * 10;
+    boxSize = basicSize + i * 10;
     const div = document.createElement("div");
-    div.style.cssText = `width: ${size}px; height: ${size}px; margin: 5px; background-color: rgba( ${random()} , ${random()} , ${random()} )`;
+    div.style.cssText = `width: ${boxSize}px; height: ${boxSize}px; margin: 5px; background-color: rgba( ${random()} , ${random()} , ${random()} )`;
     fragment.appendChild(div);
   }
-  
+  basicSize = boxSize + 10;
   boxes.appendChild(fragment);
 }
 
 function destroyBoxes() {
   boxes.innerHTML = "";
+  basicSize = 30;
   }
 
 function random() {
